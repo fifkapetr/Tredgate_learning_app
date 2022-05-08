7e37726dec8af2b16ece28d5ee74443e717f5f3d
Petr Fifka <petrfifka@Petrs-MacBook-Pro-2.local>
2022-05-08 18:02:18 +0200

    Login logic, logged page

---
+++ app/src/main/java/com/example/tredgate_learningapp/LoggedUser.java
@@ -0,0 +1,14 @@
+package com.example.tredgate_learningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoggedUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_user);
+    }
+}
