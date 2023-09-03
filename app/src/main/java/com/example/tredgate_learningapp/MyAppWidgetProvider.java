022eb65004487676e24148e7ffd5b8bd1f19979d
Petr Fifka <petr.fifka@tredgate.com>
2023-09-03 04:20:52 +0200

    add tredgate widget

---
+++ app/src/main/java/com/example/tredgate_learningapp/MyAppWidgetProvider.java
@@ -0,0 +1,32 @@
+package com.example.tredgate_learningapp;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class MyAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+        // Create an intent to open the app when button is clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        // Associate the button click with the pending intent
+        views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+
+        // Update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
